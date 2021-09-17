c4e3602b929a7c6b22cda6d2f1ca468e02b62125
njokuifeanyigerald <brainbboyrichmond@gmail.com>
2021-09-17 11:08:07 +0100

    added delete func

---
+++ website/static/app.js
@@ -0,0 +1,9 @@
+function deleteNote(noteId){
+    fetch('/delete', {
+        method: 'POST',
+        body: JSON.stringify({noteId:noteId}),
+    }).then((_res) => {
+        // window.location.href = '/';
+        location.reload()
+    })
+}
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, request
+from flask import Blueprint, render_template, flash, request, jsonify
 from flask.helpers import url_for
 # blueprint helps separete views
 from flask_login import login_required, current_user
@@ -6,14 +6,14 @@ from werkzeug.utils import redirect
 views = Blueprint('views', __name__)
 from . import db
 from .models import Note
-from sqlalchemy import desc
+from sqlalchemy import desc #to it will go in descending order
 
 @views.route('/', methods=['GET','POST'])
 @login_required
 def home():
     # for reference purpose
     # there are 2 method to pull data from the current user either this way or 
-    # the 'user.note' way which works with current_user which will be added to the home.html
+    # the 'user.notes' way which works with current_user which will be added to the home.html
     my_notes = Note.query.filter_by(user_id=current_user.id).order_by(desc(Note.date))
     if request.method == 'POST':
         note = request.form.get('note')
@@ -26,3 +26,19 @@ def home():
             flash('Note successfully added', category='success')
             return redirect(url_for('views.home'))
     return render_template('home.html', user=current_user, my_notes=my_notes)
+
+
+@views.route('/delete', methods=['POST'])
+@login_required
+def delete():
+    import json
+
+    data = json.loads(request.data)
+    noteId = data['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    # it not a must that someone must return something
+    return jsonify({'code': 'deleted successfully'})
